79a7f1b5128a368574bb810ecb77b906f10dcf05
Rest <code.rest.dev@gmail.com>
2024-01-12 23:22:52 -0300

    Javascript en Node.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello World")
+// let username = "Rest"
+// let age = 21
+// let hasHobbies = false
+// let points = [10,20,30]
+// let user = {
+//     name : "Kevin",
+//     lastname : "Alejandro"
+// }
+// const PI = 3.146
+
+// console.log(username)
+// console.log(age)
+// console.log(hasHobbies)
+// console.log(points)
+// console.log(user)
+// console.log(PI)
+
+// const age = 13 
+
+// if (age >= 18){
+//     console.log("u are an adult")
+// }else if(age >= 13){
+//     console.log(" u are a teenager")
+// }
+// else {
+//     console.log("u are a child")
+// }
+
+
+// const names = ["joe", "kevin", "marco"]
+
+// for(var i = 0; i < names.length; i++){
+//     console.log(names[i])
+// }
+
+const showUserInfo = (userName, userAge) => `The username is ${userName}, the user is ${userAge} years old`
+
+
+console.log(showUserInfo("Matias",21))
+console.log(showUserInfo("Maria",50))
