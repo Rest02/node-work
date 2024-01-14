ca146b5e9300c335e92a8f727b0acd45547f85fd
Rest <code.rest.dev@gmail.com>
2024-01-14 20:36:20 -0300

    Primer modulo

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const data = require("./modules/myModules")
+const math  = require("./math/index")
 
-console.log(data)
+
+console.log(math)
+
+console.log(math.sumar(10,20))
+console.log(math.restar(10,20))
+console.log(math.dividir(10,20))
+console.log(math.multiplicar(10,20))
---
+++ math/index.js
@@ -0,0 +1,22 @@
+function sumar(x, b) {
+  return x + b;
+}
+
+function restar(x, b) {
+  return x - b;
+}
+
+function multiplicar(x, b) {
+  return x * b;
+}
+
+function dividir(x, b) {
+  return x / b;
+}
+
+module.exports = {
+  sumar,
+  restar,
+  multiplicar,
+  dividir,
+};
