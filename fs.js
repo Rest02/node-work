d61c7052556c5c915710d5a083eb6748049a3d4b
Rest <code.rest.dev@gmail.com>
2024-01-16 22:29:53 -0300

    Modulo fs (async)

--- fs.js
+++ fs.js
@@ -1,13 +1,38 @@
-const fs = require("fs")
+const fs = require("fs");
 
-const first = fs.readFileSync("./data/first.txt", "utf-8")
-const second = fs.readFileSync("./data/second.txt")
+// const first = fs.readFileSync("./data/first.txt", "utf-8")
+// const second = fs.readFileSync("./data/second.txt")
 
-console.log(first)
-console.log(second.toString())
+// console.log(first)
+// console.log(second.toString())
 
-const title = "este  un nuevo  archivo"
+// const title = "este  un nuevo  archivo"
 
-fs.writeFileSync("./data/four.txt", title,{
-    flag: "a"
-})
+// fs.writeFileSync("./data/four.txt", title,{
+//     flag: "a"
+// })
+
+fs.readFile("./data/first.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+
+  fs.readFile("./data/second.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(data);
+
+    fs.writeFile(
+      "./data/newfile.txt",
+      "Archivo creado desde fs",
+      (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(data);
+      }
+    );
+  });
+});
