ec9f7b10f95573a32e3ea2d1b3979359329ee428
Rest <code.rest.dev@gmail.com>
2024-01-17 18:29:11 -0300

    Http request

--- http.js
+++ http.js
@@ -1,8 +1,26 @@
-const http = require("http")
+const http = require("http");
 
-http.createServer((request, response) => {
-    response.write("Hello World")
-    response.end()
-}).listen(3000)
+const server = http.createServer((request, response) => {
+  console.log(request);
 
-console.log("Servidor escuchando en el puerto 3000")
+  if (request.url == "/") {
+    response.write("welcome to the server");
+    return response.end();
+  }
+
+  if (request.url == "/about") {
+    response.write("acerca de");
+    return response.end();
+  }
+
+  response.write(`
+        <h1> Not Found </h1>
+        <p> Esta página no se encontro </p>
+        <a href = "/" > Volver a la pagina principal </a>
+
+    `);
+  response.end();
+});
+
+server.listen(3000);
+console.log("Servidor escuchando en el puerto 3000");
