075c5275bd9ee86e16ce07dade12fbda22fe8393
Rest <code.rest.dev@gmail.com>
2024-01-22 22:04:36 -0300

    async/await

--- promise.js
+++ promise.js
@@ -1,24 +1,36 @@
-const {readFile} = require("fs")
+const { readFile } = require("fs");
 
+const getText = (pathFile) => {
+  return new Promise(function (resolve, reject) {
+    readFile(pathFile, "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
 
-const getText = (pathFile)=>{
-    return new Promise(function(resolve, reject){
-        readFile(pathFile, "utf-8", (err, data)=>{
-            if(err){
-                reject(err)
-        
-            }
-            resolve(data)
-        })  
-    })
+// getText("./data/four.txt")
+//     .then(result=>console.log(result))
+//     .then(()=>("./data/first.txt"))
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error))
+
+async function read() {
+  try {
+    const result = await getText("./data/first.txt");
+    const result2 = await getText("./data/second.txt");
+    const result3 = await getText("./data/third.txt");
+    const result4 = await getText("./data/four.txt");
+    console.log(result);
+    console.log(result2);
+    console.log(result3);
+    console.log(result4);
+    throw new Error("esto es un error que no se esperaba")
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-
-
-
-
-getText("./data/four.txt")
-    .then(result=>console.log(result))
-    .then(()=>("./data/first.txt"))
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
+read();
