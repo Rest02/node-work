32bf6e3f727e7abbdff114db6d42ed9f73c75b4d
Rest <code.rest.dev@gmail.com>
2024-01-24 17:30:10 -0300

    fs Promises

--- promise.js
+++ promise.js
@@ -1,36 +1,16 @@
-const { readFile } = require("fs");
-const {promisify} =  require("util")
+const { readFile } = require("fs/promises");
 
-const readFilePromise = promisify(readFile)
-
-// const getText = (pathFile) => {
-//   return new Promise(function (resolve, reject) {
-//     readFile(pathFile, "utf-8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       }
-//       resolve(data);
-//     });
-//   });
-// };
-
-// getText("./data/four.txt")
-//     .then(result=>console.log(result))
-//     .then(()=>("./data/first.txt"))
-//     .then(result => console.log(result))
-//     .catch(error => console.log(error))
 
 async function read() {
   try {
-    const result = await readFilePromise("./data/first.txt", "utf-8");
-    const result2 = await readFilePromise("./data/second.txt", "utf-8");
-    const result3 = await readFilePromise("./data/third.txt", "utf-8");
-    const result4 = await readFilePromise("./data/four.txt", "utf-8");
+    const result = await readFile("./data/first.txt", "utf-8");
     console.log(result);
+    const result2 = await readFile("./data/second.txt","utf-8");
     console.log(result2);
+    const result3 = await readFile("./data/third.txt","utf-8");
     console.log(result3);
+    const result4 = await readFile("./data/four.txt","utf-8");
     console.log(result4);
-    throw new Error("esto es un error que no se esperaba")
   } catch (error) {
     console.log(error);
   }
