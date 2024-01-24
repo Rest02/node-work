185b054b4479f39fe46dcbec81da07bfb1ce75f3
Rest <code.rest.dev@gmail.com>
2024-01-23 21:22:03 -0300

    Promisify

--- promise.js
+++ promise.js
@@ -1,15 +1,18 @@
 const { readFile } = require("fs");
+const {promisify} =  require("util")
 
-const getText = (pathFile) => {
-  return new Promise(function (resolve, reject) {
-    readFile(pathFile, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-    });
-  });
-};
+const readFilePromise = promisify(readFile)
+
+// const getText = (pathFile) => {
+//   return new Promise(function (resolve, reject) {
+//     readFile(pathFile, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
+// };
 
 // getText("./data/four.txt")
 //     .then(result=>console.log(result))
@@ -19,10 +22,10 @@ const getText = (pathFile) => {
 
 async function read() {
   try {
-    const result = await getText("./data/first.txt");
-    const result2 = await getText("./data/second.txt");
-    const result3 = await getText("./data/third.txt");
-    const result4 = await getText("./data/four.txt");
+    const result = await readFilePromise("./data/first.txt", "utf-8");
+    const result2 = await readFilePromise("./data/second.txt", "utf-8");
+    const result3 = await readFilePromise("./data/third.txt", "utf-8");
+    const result4 = await readFilePromise("./data/four.txt", "utf-8");
     console.log(result);
     console.log(result2);
     console.log(result3);
