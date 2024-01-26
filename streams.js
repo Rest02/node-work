92a7db11ce04178773a296d5b842f9a50a5d68ee
Rest <code.rest.dev@gmail.com>
2024-01-25 23:19:45 -0300

    streams

---
+++ streams.js
@@ -0,0 +1,26 @@
+// const {writeFile} = require("fs/promises")
+
+
+// const createBigFile = async () => {
+//     await writeFile("./data/bigFile.txt", "hello world".repeat(1000000))
+// }
+
+// createBigFile()
+
+const {createReadStream} = require("fs")
+
+
+const stream = createReadStream("./data/bigFile.txt", "utf-8")
+
+stream.on("data", (chunk) => {
+    console.log(chunk)
+})
+
+
+stream.on("end", ()=>{
+    console.log("Ya termine de leer el archivo")
+})
+
+stream.on("error", (error)=>{
+    console.log(error)
+})
